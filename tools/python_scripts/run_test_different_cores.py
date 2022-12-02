583b2817f709f433f602449ee1ccf6ce1385a82d
Sebastiano Miano <mianosebastiano@gmail.com>
2022-12-02 12:24:03 +0000

    Add action in stats filename

--- tools/python_scripts/run_test_different_cores.py
+++ tools/python_scripts/run_test_different_cores.py
@@ -195,7 +195,7 @@ def main():
                     logger.critical("*** Caught exception: %s: %s" % (e.__class__, e))
                     sys.exit(1)
 
-                stats_file_name = f"result_{version}_core{core}_run{run}.csv"
+                stats_file_name = f"result_{version}_core{core}_run{run}_{action}.csv"
                 init_remote_client(client, remote_conntrack_path, remote_iface, core, version, action, duration, f"{remote_conntrack_path}/src/{stats_file_name}")
 
                 if action == "DROP":
