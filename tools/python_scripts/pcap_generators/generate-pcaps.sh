e1377b80c7d1e16c38e1d93eff65207f08c50e81
Sebastiano Miano <mianosebastiano@gmail.com>
2022-12-09 11:48:55 +0000

    Add script to generate pcaps

--- tools/python_scripts/pcap_generators/generate-pcaps.sh
+++ tools/python_scripts/pcap_generators/generate-pcaps.sh
@@ -3,11 +3,11 @@
 NUM_CORES=6
 
 echo "Generating PCAP for v1"
-for i in {1..${NUM_CORES}}; do
+for ((i=1;i<=NUM_CORES;i++)); do
     python3 generate_pcap_conntrack_v1.py -o conntrack_v1_core${i}.pcap -r -n ${i}
 done
 
 echo "Generating PCAP for v2"
-for i in {1..${NUM_CORES}}; do
+for ((i=1;i<=NUM_CORES;i++)); do
     python3 generate_pcap_conntrack_v2.py -o conntrack_v2_core${i}.pcap -r -n ${i}
 done
