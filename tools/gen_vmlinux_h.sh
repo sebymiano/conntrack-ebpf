b7ff348850713a83d087a84cc783840432194b01
Sebastiano Miano <mianosebastiano@gmail.com>
2023-09-19 11:36:22 +0100

    Update vmlinux.h generation

--- tools/gen_vmlinux_h.sh
+++ tools/gen_vmlinux_h.sh
@@ -1,3 +1,11 @@
 #/bin/sh
 
-$(dirname "$0")/bpftool btf dump file ${1:-/sys/kernel/btf/vmlinux} format c
+# Check if there is a argument passed to this script, if yes, use it as the path to bpftool
+if [ $# -eq 0 ]
+  then
+    bpftool_exec=bpftool
+  else
+    bpftool_exec=$1
+fi
+
+$bpftool_exec btf dump file ${2:-/sys/kernel/btf/vmlinux} format c > $(dirname "$0")/../vmlinux/vmlinux.h
